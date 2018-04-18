4569ce65ad68dd45e141d7c185baf00bcf25491c
hemin123 <1565417293@qq.com>
2018-04-18 17:24:01 +0800

    4.18

---
+++ mi -dazuoyejs/mi/js/index.js
@@ -0,0 +1,28 @@
+
+// window.onload = function(){
+
+var odiv1 =document.getElementById('one');
+var odiv2 =document.getElementById('two');
+
+var obtnshu1 =document.getElementById('btnshu1');
+var obrnshu2 =document.getElementById('btnshu2');
+var obtnshu3 =document.getElementById('brnshu3');
+alert('进入');
+obtnshu1.onclick = function(){
+	odiv2.style.display='none';
+	odiv1.style.display ='block';
+}
+obtnshu2.onclick = function(){
+	odiv1.style.display='none';
+	odiv2.style.display ='block';
+// }
+
+
+
+		
+
+
+
+
+
+}
